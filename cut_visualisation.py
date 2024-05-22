515ca9cbf5a2359b061006cd91842193529ffd99
Olexandr Gugnin <alexgugnin21@gmail.com>
2024-05-22 23:26:55 +0300

    Added function for ortogonal transformation of the object's plane and adjusted distance criterion for crossing the plane, also added the cut visualisator program

---
+++ cut_visualisation.py
@@ -0,0 +1,152 @@
+from crpropa import *
+from useful_funcs import eqToGal
+import numpy as np
+from tqdm import tqdm
+import pandas as pd
+import matplotlib.pyplot as plt
+from side_checks.calc_metric_for_seed_check import makeCut, calcPerpPlane, calcEdge, r_mat, calculate_kde
+
+import glob
+
+def plot3D(data) -> None:
+    fig = plt.figure(figsize=(12,12))
+    ax = plt.subplot(111, projection='3d')
+
+    # plot trajectories
+    I,X,Y,Z = data
+    for i in np.unique(I):
+        if i > 50: break
+        ax.plot(X[I == i], Y[I == i], Z[I == i], lw=1, alpha=1, c='g')
+
+    # plot Galactic border
+    r = 20
+    u, v = np.meshgrid(np.linspace(0, 2*np.pi, 100), np.linspace(0, np.pi, 100))
+    x = r * np.cos(u) * np.sin(v)
+    y = r * np.sin(u) * np.sin(v)
+    z = r * np.cos(v)
+    ax.plot_surface(x, y, z, rstride=2, cstride=2, color='r', alpha=0.1, lw=0)
+    ax.plot_wireframe(x, y, z, rstride=10, cstride=10, color='k', alpha=0.5, lw=0.1)
+
+    # plot Galactic center
+    ax.scatter(0,0,0, marker='o', color='r')
+    # plot Earth
+    ax.scatter(-8.5,0,0, marker='P', color='b')
+
+    #Plotting potential sources
+    #Plot SGR 1900+14
+    ax.scatter(12.5*np.cos(43.02*np.pi/180)*np.cos(0.77*np.pi/180) - 8.5, 12.5*np.sin(43.02*np.pi/180)*np.cos(0.77*np.pi/180), 12.5*np.sin(0.77*np.pi/180), marker='+', c='red', s=70) #43.02 0.77 12.5±1.7
+    #plt.text(0.751-2*0.751 - 15*np.pi/180, 0.0135+7*np.pi/180, 'SGR 1900+14', fontsize=8, fontweight='bold')
+    #Plot GRS 1915+105
+    ax.scatter(8.6*np.cos(45.37*np.pi/180)*np.cos(-0.22*np.pi/180) - 8.5, 8.6*np.sin(45.37*np.pi/180)*np.cos(-0.22*np.pi/180), 8.6*np.sin(-0.22*np.pi/180), marker='+', c='green', s=70) #45.37 -0.22 8.6+2.0-1.6
+    #Plot SS 433 Мікроквазар 39.69 -2.24 5.5±0.2
+    ax.scatter(5.5*np.cos(39.69*np.pi/180)*np.cos(-2.24*np.pi/180) - 8.5, 5.5*np.sin(39.69*np.pi/180)*np.cos(-2.24*np.pi/180), 5.5*np.sin(-2.24*np.pi/180), marker='+', c='purple', s=70) #39.69 -2.24 5.5±0.2
+    #Plot NGC 6760 Кулясте скупчення 36.11 -3.9 7.4±0.4
+    ax.scatter(7.4*np.cos(36.11*np.pi/180)*np.cos(-3.9*np.pi/180) - 8.5, 7.4*np.sin(36.11*np.pi/180)*np.cos(-3.9*np.pi/180), 7.4*np.sin(-3.9*np.pi/180), marker='+', c='magenta', s=70) #36.11 -3.9 7.4±0.4
+
+    from matplotlib.lines import Line2D
+    legend_elements = [Line2D([0], [0], color='blue', lw=1, label='Simulated CRs'),
+                        Line2D([0], [0], marker='+', color='red', label='SGR 1900+14', markerfacecolor='red', linestyle='', markersize=8),
+                        Line2D([0], [0], marker='+', color='green', label='GRS 1915+105', markerfacecolor='green', linestyle='', markersize=8),
+                        Line2D([0], [0], marker='+', color='purple', label='SS 433', markerfacecolor='purple', linestyle='', markersize=8),
+                        Line2D([0], [0], marker='+', color='magenta', label='NGC 6760', markerfacecolor='magenta', linestyle='', markersize=8)
+                        ]
+    fig.legend(handles=legend_elements, loc='upper right')
+
+    ax.tick_params(axis='both', which='major', labelsize=16)
+    ax.tick_params(axis='both', which='minor', labelsize=16)
+    ax.set_xlabel('x / kpc', fontsize=18)
+    ax.set_ylabel('y / kpc', fontsize=18)
+    ax.set_zlabel('z / kpc', fontsize=18)
+    ax.set_xlim((-20, 20))
+    ax.set_ylim((-20, 20))
+    ax.set_zlim((-20, 20))
+    ax.xaxis.set_ticks((-20,-10,0,10,20))
+    ax.yaxis.set_ticks((-20,-10,0,10,20))
+    ax.zaxis.set_ticks((-20,-10,0,10,20))
+    plt.show()
+
+def plot3D_from_pandas(data, data_cut):
+    fig = plt.figure(figsize=(12,12))
+    ax = plt.subplot(111, projection='3d')
+
+    # plot trajectories
+    I,X,Y,Z = data
+    for i in np.unique(I):
+        if i > 50: break
+        ax.plot(X[I == i], Y[I == i], Z[I == i], lw=1, alpha=0.2, c='g')
+    #Plot cut surface
+    ax.scatter(data_cut['X'], data_cut['Y'], data_cut['Z'], lw=1, alpha=1, c='r', s=10)
+
+    # plot Galactic border
+    r = 20
+    u, v = np.meshgrid(np.linspace(0, 2*np.pi, 100), np.linspace(0, np.pi, 100))
+    x = r * np.cos(u) * np.sin(v)
+    y = r * np.sin(u) * np.sin(v)
+    z = r * np.cos(v)
+    ax.plot_surface(x, y, z, rstride=2, cstride=2, color='r', alpha=0.1, lw=0)
+    ax.plot_wireframe(x, y, z, rstride=10, cstride=10, color='k', alpha=0.5, lw=0.1)
+
+    # plot Galactic center
+    ax.scatter(0,0,0, marker='o', color='r')
+    # plot Earth
+    ax.scatter(-8.5,0,0, marker='P', color='b')
+
+    #Plotting potential sources
+    #Plot SGR 1900+14
+    sgr_cords = [0, 12.5*np.cos(43.02*np.pi/180)*np.cos(0.77*np.pi/180) - 8.5, 12.5*np.sin(43.02*np.pi/180)*np.cos(0.77*np.pi/180), 12.5*np.sin(0.77*np.pi/180)]
+    ax.scatter(12.5*np.cos(43.02*np.pi/180)*np.cos(0.77*np.pi/180) - 8.5, 12.5*np.sin(43.02*np.pi/180)*np.cos(0.77*np.pi/180), 12.5*np.sin(0.77*np.pi/180), marker='+', c='b', s=70) #43.02 0.77 12.5±1.7
+    #Transformed SGR 1900+14
+    norm, plane = calcPerpPlane(sgr_cords)
+    sgr_trans = np.array(sgr_cords[1:]).reshape(-1,1)
+    abs_norm = np.sqrt(norm[0]**2 + norm[1]**2 + norm[2]**2)
+    alpha = np.arccos(norm[0]/abs_norm)
+    sgr_rot = np.matmul(r_mat(-alpha), sgr_trans)
+    ax.scatter(sgr_rot[0], sgr_rot[1], sgr_rot[2], marker='+', c='b', s=70)
+    #plt.text(0.751-2*0.751 - 15*np.pi/180, 0.0135+7*np.pi/180, 'SGR 1900+14', fontsize=8, fontweight='bold')
+    #Plot GRS 1915+105
+    ax.scatter(8.6*np.cos(45.37*np.pi/180)*np.cos(-0.22*np.pi/180) - 8.5, 8.6*np.sin(45.37*np.pi/180)*np.cos(-0.22*np.pi/180), 8.6*np.sin(-0.22*np.pi/180), marker='+', c='green', s=70) #45.37 -0.22 8.6+2.0-1.6
+    #Plot SS 433 Мікроквазар 39.69 -2.24 5.5±0.2
+    ax.scatter(5.5*np.cos(39.69*np.pi/180)*np.cos(-2.24*np.pi/180) - 8.5, 5.5*np.sin(39.69*np.pi/180)*np.cos(-2.24*np.pi/180), 5.5*np.sin(-2.24*np.pi/180), marker='+', c='purple', s=70) #39.69 -2.24 5.5±0.2
+    #Plot NGC 6760 Кулясте скупчення 36.11 -3.9 7.4±0.4
+    ax.scatter(7.4*np.cos(36.11*np.pi/180)*np.cos(-3.9*np.pi/180) - 8.5, 7.4*np.sin(36.11*np.pi/180)*np.cos(-3.9*np.pi/180), 7.4*np.sin(-3.9*np.pi/180), marker='+', c='magenta', s=70) #36.11 -3.9 7.4±0.4
+
+    from matplotlib.lines import Line2D
+    legend_elements = [Line2D([0], [0], color='blue', lw=1, label='Simulated CRs'),
+                        Line2D([0], [0], marker='+', color='red', label='SGR 1900+14', markerfacecolor='red', linestyle='', markersize=8),
+                        Line2D([0], [0], marker='+', color='green', label='GRS 1915+105', markerfacecolor='green', linestyle='', markersize=8),
+                        Line2D([0], [0], marker='+', color='purple', label='SS 433', markerfacecolor='purple', linestyle='', markersize=8),
+                        Line2D([0], [0], marker='+', color='magenta', label='NGC 6760', markerfacecolor='magenta', linestyle='', markersize=8)
+                        ]
+    fig.legend(handles=legend_elements, loc='upper right')
+
+    ax.tick_params(axis='both', which='major', labelsize=16)
+    ax.tick_params(axis='both', which='minor', labelsize=16)
+    ax.set_xlabel('x / kpc', fontsize=18)
+    ax.set_ylabel('y / kpc', fontsize=18)
+    ax.set_zlabel('z / kpc', fontsize=18)
+    ax.set_xlim((-20, 20))
+    ax.set_ylim((-20, 20))
+    ax.set_zlim((-20, 20))
+    ax.xaxis.set_ticks((-20,-10,0,10,20))
+    ax.yaxis.set_ticks((-20,-10,0,10,20))
+    ax.zaxis.set_ticks((-20,-10,0,10,20))
+    plt.show()
+
+if __name__ == '__main__':
+    data = np.genfromtxt('traj_PA+TA_Fe_22_event_10737418_seed.txt', unpack=True, skip_footer=1)
+    sgr_cords = [0, 12.5*np.cos(43.02*np.pi/180)*np.cos(0.77*np.pi/180) - 8.5, 12.5*np.sin(43.02*np.pi/180)*np.cos(0.77*np.pi/180), 12.5*np.sin(0.77*np.pi/180)]
+    data_cut = makeCut(data, sgr_cords)
+    plot3D_from_pandas(data, data_cut)
+    score = calculate_kde(data_cut, sgr_cords)
+    plt.scatter(data_cut['Y'], data_cut['Z'], s = 5)
+    norm, plane = calcPerpPlane(sgr_cords)
+    sgr_trans = np.array(sgr_cords[1:]).reshape(-1,1)
+    abs_norm = np.sqrt(norm[0]**2 + norm[1]**2 + norm[2]**2)
+    alpha = np.arccos(norm[0]/abs_norm)
+    sgr_rot = np.matmul(r_mat(-alpha), sgr_trans)
+    print(score)
+    plt.scatter(sgr_rot[1], sgr_rot[2])
+    plt.show()
+
+
+    #print(data_cut)
--- side_checks/calc_metric_for_seed_check.py
+++ side_checks/calc_metric_for_seed_check.py
@@ -10,8 +10,8 @@ def calcPerpPlane(target_cords, earth_cords = [0, -8.5, 0, 0]):
     '''Calculates the A, B, C, D for the plane'''
     norm = np.array([target_cords[1] - earth_cords[1], target_cords[2] - earth_cords[2], target_cords[3] - earth_cords[3]])
     D_plane = -(norm[0]*target_cords[1] + norm[1]*target_cords[2] + norm[2]*target_cords[3])
-    xx, yy = np.meshgrid(np.linspace(-20,20,160), np.linspace(-20,20,160))
-    z = (-norm[0] * xx - norm[1] * yy + D_plane)/norm[2]
+    #xx, yy = np.meshgrid(np.linspace(-20,20,160), np.linspace(-20,20,160))
+    #z = (-norm[0] * xx - norm[1] * yy + D_plane)/norm[2]
 
     return norm, D_plane
 
@@ -20,7 +20,7 @@ def calcEdge(x, y, z, norm, D_plane):
     old_distance = 1e5
     for idx in range(len(x)):
         new_distance = abs(norm[0]*x[idx] + norm[1]*y[idx] + norm[2]*z[idx] + D_plane)/np.sqrt(norm[0]**2 + norm[1]**2 + norm[2]**2)
-        if new_distance > old_distance:
+        if ((new_distance > old_distance) and (new_distance <= 0.2)):
             x = x[:idx]
             y = y[:idx]
             z = z[:idx]
@@ -28,53 +28,104 @@ def calcEdge(x, y, z, norm, D_plane):
         else:
             old_distance = new_distance
 
+def p_mat(angle):
+    '''Rotate over X'''
+    p_matrix = np.array([
+        [1,           0,                 0],
+        [0, np.cos(angle),   -np.sin(angle)],
+        [0, np.sin(angle),  np.cos(angle)]
+    ])
+    return p_matrix
+
+def q_mat(angle):
+    '''Rotate over Y'''
+    q_matrix = np.array([
+        [np.cos(angle), 0, np.sin(angle)],
+        [0,             1,              0],
+        [-np.sin(angle), 0,  np.cos(angle)]
+    ])
+    return q_matrix
+
+def r_mat(angle):
+    '''Rotate over Z'''
+    r_matrix = np.array([
+        [np.cos(angle), -np.sin(angle), 0],
+        [np.sin(angle), np.cos(angle),   0],
+        [0,               0,           1]
+    ])
+    return r_matrix
+
+
+def rotate_yz(x, y, z, norm):
+    '''Ortogonal transformation of a plane'''
+    abs_norm = np.sqrt(norm[0]**2 + norm[1]**2 + norm[2]**2)
+    alpha = np.arccos(norm[0]/abs_norm)
+    beta = np.arccos(norm[1]/abs_norm)
+    gamma = np.arccos(norm[2]/abs_norm)
+    data_before_rot = np.vstack((x,y,z))
+    data_after_rot = np.matmul(r_mat(-alpha), data_before_rot)
+
+    return data_after_rot[:1].ravel(), data_after_rot[1:2].ravel(), data_after_rot[2:].ravel()
+
 def makeCut(data, target_cords):
     '''Returns the point of intersection of the trajectory with the object surface.
-    Doesnt make ortogonal transformation'''
+    Doesnt make ortogonal transformation. Need to be merged with calcEdge?'''
     I,X,Y,Z = data
     x_nonrot, y_nonrot, z_nonrot = [], [], []
     for i in np.unique(I):
         norm, D_plane = calcPerpPlane(target_cords)
-        _x, _y, _z = calcEdge(X[I == i], Y[I == i], Z[I == i], norm, D_plane)
+        try:
+            _x, _y, _z = calcEdge(X[I == i], Y[I == i], Z[I == i], norm, D_plane)
+        except:
+            continue
         x_nonrot.append(_x[-1])
         y_nonrot.append(_y[-1])
         z_nonrot.append(_z[-1])
 
-    return pd.DataFrame({'X':np.array(x_nonrot), 'Y':np.array(y_nonrot), 'Z':np.array(z_nonrot)})
+    x_rot, y_rot, z_rot = rotate_yz(x_nonrot, y_nonrot, z_nonrot, norm)
+
+    return pd.DataFrame({'X':np.array(x_rot), 'Y':np.array(y_rot), 'Z':np.array(z_rot)})
 
 def calculate_kde(data, object_cords) -> float:
-  '''Calculates pdf using kde with bandwith from the gridsearch
-  and returns the denstiy value of needed object for this pdf divided by
-  the max density value for this pdf
-  https://gist.github.com/daleroberts/7a13afed55f3e2388865b0ec94cd80d2
-  https://jakevdp.github.io/blog/2013/12/01/kernel-density-estimation/'''
-  xy = np.vstack([data['Y'], data['Z']])
-  d = xy.shape[0]
-  n = xy.shape[1]
+    '''Calculates pdf using kde with bandwith from the gridsearch
+    and returns the denstiy value of needed object for this pdf divided by
+    the max density value for this pdf
+    https://gist.github.com/daleroberts/7a13afed55f3e2388865b0ec94cd80d2
+    https://jakevdp.github.io/blog/2013/12/01/kernel-density-estimation/'''
+    xy = np.vstack([data['Y'], data['Z']])
+    d = xy.shape[0]
+    n = xy.shape[1]
 
-  #Creating grid for search and finding best estimator in terms of bandwidth
-  grid = GridSearchCV(KernelDensity(),
+    #Creating grid for search and finding best estimator in terms of bandwidth
+    grid = GridSearchCV(KernelDensity(),
                     {'bandwidth': np.linspace(0.01, 1.0, 100)},
                     cv=20) # 20-fold cross-validation with 100 bandwidths
-  grid.fit(xy.T)
-  kde = grid.best_estimator_
-  point = np.array([[object_cords[2]], [object_cords[3]]])
+    grid.fit(xy.T)
+    kde = grid.best_estimator_
 
-  xmin = data['Y'].min()
-  xmax = data['Y'].max()
-  ymin = data['Z'].min()
-  ymax = data['Z'].max()
+    #Rotating object
+    norm, plane = calcPerpPlane(object_cords)
+    obj_trans = np.array(object_cords[1:]).reshape(-1,1)
+    abs_norm = np.sqrt(norm[0]**2 + norm[1]**2 + norm[2]**2)
+    alpha = np.arccos(norm[0]/abs_norm)
+    obj_rot = np.matmul(r_mat(-alpha), obj_trans)
+    point = np.array([obj_rot[1], obj_rot[2]])
 
-  X, Y = np.mgrid[xmin:xmax:100j, ymin:ymax:100j]
-  positions = np.vstack([X.ravel(), Y.ravel()])
+    xmin = data['Y'].min()
+    xmax = data['Y'].max()
+    ymin = data['Z'].min()
+    ymax = data['Z'].max()
 
-  Z = np.reshape(np.exp(kde.score_samples(positions.T)), X.shape)
+    X, Y = np.mgrid[xmin:xmax:100j, ymin:ymax:100j]
+    positions = np.vstack([X.ravel(), Y.ravel()])
 
-  return (np.exp(kde.score_samples(point.T))/Z.max())[0] #score_samples returns the log density, so exp is needed. Also prob density can be more than 1
+    Z = np.reshape(np.exp(kde.score_samples(positions.T)), X.shape)
+
+    return (np.exp(kde.score_samples(point.T))/Z.max())[0] #score_samples returns the log density, so exp is needed. Also prob density can be more than 1
 
 
 if __name__ == '__main__':
-    path = 'trajectories_1e3_1000_rand_seeds/H/'
+    path = 'trajectories_1e3_1000_rand_seeds/Fe/'
     file_list = glob.glob(f'{path}*.txt')
     objects_list = {
         "sgr": [0, 12.5*np.cos(43.02*np.pi/180)*np.cos(0.77*np.pi/180) - 8.5, 12.5*np.sin(43.02*np.pi/180)*np.cos(0.77*np.pi/180), 12.5*np.sin(0.77*np.pi/180)],
@@ -102,5 +153,4 @@ if __name__ == '__main__':
             temp_df = pd.DataFrame(temp_dict)
             result = pd.concat([result, temp_df], ignore_index = True)
             result.reset_index()
-        break
     result.to_csv(f"{path}results_for_{path.split('/')[1]}.csv")
